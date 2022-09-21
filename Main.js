0a45ca16354e5cb5de9a5eb7b06cffa741f0c87a
Marcus <36953351+chenjiadong19951207@users.noreply.github.com>
2022-09-21 13:31:43 +0800

    3-5

--- Main.js
+++ Main.js
@@ -1,5 +1,13 @@
+import {ResourceLoader} from "./js/base/ResourceLoader.js";
+
 export class Main{
     constructor() {
-        console.log('我执行拉');
+        this.canvas = document.getElementById('game_canvas');
+        this.ctx = this.canvas.getContext('2d');
+        const loader = ResourceLoader.create();
+        loader.onloaded(map=> this.onResourceFirstLoaded(map));
+    }
+    onResourceFirstLoaded(map){
+
     }
 }
