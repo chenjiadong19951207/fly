7bc7695103b0f24805b8e4d9e0a1f5b963f174c1
Marcus <36953351+chenjiadong19951207@users.noreply.github.com>
2022-09-22 13:54:12 +0800

    4-3

--- Main.js
+++ Main.js
@@ -1,4 +1,5 @@
 import {ResourceLoader} from "./js/base/ResourceLoader.js";
+import {BackGround} from "./js/runtime/BackGround.js";
 
 export class Main{
     constructor() {
@@ -6,8 +7,26 @@ export class Main{
         this.ctx = this.canvas.getContext('2d');
         const loader = ResourceLoader.create();
         loader.onloaded(map=> this.onResourceFirstLoaded(map));
+
+        let image = new Image();
+        image.src = '../res/background.png';
+        image.onload = () => {
+            this.ctx.drawImage(
+                image,
+                0,
+                0,
+                image.width,
+                image.height,
+                0,
+                0,
+                image.width,
+                image.height
+            );
+        }
+
     }
     onResourceFirstLoaded(map){
-
+        let background = new BackGround(this.ctx,map.get('background'));
+        background.draw();
     }
 }
--- js/Director.js
+++ js/Director.js
@@ -1,3 +1,13 @@
 export class Director{
-    
+
+    constructor() {
+
+    }
+
+    static getInstance(){
+        if(!Director.instance){
+            Director.instance = new Director();
+        }
+        return Director.instance;
+    }
 }
